1484624e33624f11df3d0cd2f48d3e9b3d31cb47
bodog <bodog@DESKTOP-39D644H.home>
2018-10-14 19:56:50 +0200

    abc

---
+++ EAI_Inventory/src/main/java/com/inventory/data/Order.java
@@ -0,0 +1,9 @@
+package com.inventory.data;
+
+public class Order {
+
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ EAI_Inventory/src/main/java/com/inventory/data/Product.java
@@ -0,0 +1,9 @@
+package com.inventory.data;
+
+public class Product {
+
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
